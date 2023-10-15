f8cebe785d60538a01fdfe9bfbdd211e038be99e
willidragon <willidragon@gmail.com>
2023-10-15 19:03:44 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,6 @@
 //create web server
-//create a server object:
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var q = url.parse(req.url, true).query;
-  var txt = q.name + " " + q.comment;
-  fs.appendFile('comments.txt', txt, function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
-  res.write('<a href="index.html">Back to home</a>');
-  res.end();
-}).listen(8080);
+
+function helloworld(req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello World\n');
+}
